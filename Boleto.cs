d122c19260217e592d54626a394b3f46159a8684
Marcio <mwm.junior@hotmail.com>
2023-05-02 08:45:09 -0300

    version 1.0

--- Boleto.cs
+++ Boleto.cs
@@ -3,5 +3,28 @@ namespace PaymentSystem
     public class Boleto
     {
         
+        // Inicializar objeto da classe Random
+        Random random = new Random();
+
+        // Utilizar herença da classe Pagamento para obter o valor do pagamento 
+
+            
+
+        // Calcular o valor do desconto
+        double desconto = valorPago * 0.12;
+        double valorComDesconto = valorPago - desconto;
+
+        // Gerar valores aleatórios para os campos do boleto
+        string banco = random.Next(100, 1000).ToString();
+        string nossoNumero = random.Next(100000000, 1000000000).ToString();
+        string fatorVencimento = random.Next(1, 10000).ToString();
+
+        // Converter os valores para strings e concatenar os campos do boleto
+        string numeroBoleto = $"{banco}{fatorVencimento}{"0000000000"}{nossoNumero}{valorComDesconto.ToString("0.00").Replace(",", "")}";
+
+        // Exibir o código de barras e valor a ser pago
+        Console.WriteLine($"Código de barras: {numeroBoleto}");
+        Console.WriteLine($"Valor a ser pago: R${valorComDesconto.ToString("0.00")}");
+
     }
 }
