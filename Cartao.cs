005c5fb9cb5db3203bb60ceede8d7e9e669fa1b8
Lucas Oliveira <lucas.gon.oliv@gmail.com>
2023-05-02 08:11:08 -0300

    Inicio metodo

--- Cartao.cs
+++ Cartao.cs
@@ -1,7 +1,15 @@
 namespace PaymentSystem
 {
-    public class Cartao
+    public abstract class Cartao
     {
-        
+        public string Bandeira { get; set; }
+        public string NumeroCartao { get; set; }
+        public string Titular { get; set; }
+        public string Cvv { get; set; }
+        public abstract void Pagar();
+        public string SalvarCartao()
+        {
+            return "Cartão salvo com sucessso!";
+        }
     }
 }
