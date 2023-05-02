7053ce23f533fed11f360c746408b2a838bee52a
Tania1912Senai <125280697+Tania1912Senai@users.noreply.github.com>
2023-05-01 23:25:12 -0300

    Update Debito.cs
    Pensei mais um pouco não sei se está correto

--- Debito.cs
+++ Debito.cs
@@ -15,6 +15,15 @@ namespace PaymentSystem
         this. numeroConta = numeroConta;
         this.titular = titular;
         }
+        // nao sei se está correto
+        public boolean sacar(double quantia) {
+        if (this.saldo < quantia ) {
+        return false;
+        } else {
+        this.saldo -= quantia;
+        return true;
+        }
+        //Nao sei se está correto
         public boolean debitar(double valor) {
         this.saldo += valor
         return true;
