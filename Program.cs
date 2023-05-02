0a86956a9bc4ac78bf1d92b5c51834835749f4df
Eduardo Alves Araújo Silva <46291995867@info.local>
2023-05-02 10:39:20 -0300

    Classe Credito quase finalizada faltando coisas simples, implementar apos testes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,2 @@
-﻿using PaymentSystem;
-
-Credito mostrar = new Credito();
-
-    mostrar.Pagar();
-
-
-
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
