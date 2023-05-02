514254383777aa64cbe7e77a7cc8bb6c015f8584
Marcio <mwm.junior@hotmail.com>
2023-05-02 10:15:49 -0300

    version 1.1

--- Boleto.cs
+++ Boleto.cs
@@ -1,30 +1,44 @@
 namespace PaymentSystem
 {
-    public class Boleto
+    public class Boleto : Pagamento  // Herença da classe Pagamento para obter o valor do pagamento 
+
     {
+
+        public string numeroBoleto { get; set; }
         
-        // Inicializar objeto da classe Random
-        Random random = new Random();
+        private string CodigoDeBaras ()
+        
+        {
 
-        // Utilizar herença da classe Pagamento para obter o valor do pagamento 
+             // Inicializar objeto da classe Random
+             Random random = new Random();
 
-            
+             // Calcular o valor do desconto
+            double desconto = valor * 0.12;
+            double valorComDesconto = valor - desconto;
 
-        // Calcular o valor do desconto
-        double desconto = valorPago * 0.12;
-        double valorComDesconto = valorPago - desconto;
+            // Gerar valores aleatórios para os campos do boleto
+            string banco = random.Next(100, 1000).ToString();
+            string nossoNumero = random.Next(100000000, 1000000000).ToString();
+            string fatorVencimento = random.Next(1, 10000).ToString();
 
-        // Gerar valores aleatórios para os campos do boleto
-        string banco = random.Next(100, 1000).ToString();
-        string nossoNumero = random.Next(100000000, 1000000000).ToString();
-        string fatorVencimento = random.Next(1, 10000).ToString();
+            // Converter os valores para strings e concatenar os campos do boleto
+             numeroBoleto = $"{banco}{fatorVencimento}{"0000000000"}{nossoNumero}{valorComDesconto.ToString("0.00").Replace(",", "")}";
 
-        // Converter os valores para strings e concatenar os campos do boleto
-        string numeroBoleto = $"{banco}{fatorVencimento}{"0000000000"}{nossoNumero}{valorComDesconto.ToString("0.00").Replace(",", "")}";
 
-        // Exibir o código de barras e valor a ser pago
-        Console.WriteLine($"Código de barras: {numeroBoleto}");
-        Console.WriteLine($"Valor a ser pago: R${valorComDesconto.ToString("0.00")}");
+            return "";
+        }
+
+        public void Registrar ()
+        {
+            // Exibir o código de barras 
+            Console.WriteLine($"Código de barras: {numeroBoleto}");
+        }
+
+
+        // // Exibir o código de barras e valor a ser pago
+        
+        // Console.WriteLine($"Valor a ser pago: R${valorComDesconto.ToString("0.00")}");
 
     }
 }
