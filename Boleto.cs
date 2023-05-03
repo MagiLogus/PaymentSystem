839d9d0f84fc15b81f77a26cf27e0ca2c26613cd
Marcio <mwm.junior@hotmail.com>
2023-05-03 10:43:19 -0300

    version 1.3

--- Boleto.cs
+++ Boleto.cs
@@ -35,14 +35,43 @@ namespace PaymentSystem
 
         public void Registrar ()
         {
+
             CodigoDeBarras();
-            
+
+            BarraCarregamento ("Registrando o boleto bancário, aguarde", 30,80);  // Chama a função da barra de carregamento
+
+            Console.Clear();
+
             // Exibir o código de barras 
+            Console.WriteLine();
             Console.WriteLine($"Código de barras: {numeroBoleto}");
             
-            // // Exibir o código de barras e valor a ser pago
+            //  Exibir o código de barras e valor a ser pago
             Console.WriteLine($"Valor a ser pago: R${valorComDesconto.ToString("0.00")}");
+            Console.WriteLine();
         }
+
+
+        public void BarraCarregamento (string texto, int quantidadePontinhos, int tempo)  // Função pontinhos da barra da carregamento
+
+        {
+            Console.Write(texto);
+          
+            
+        
+
+        for (var i= 0; i < quantidadePontinhos; i++)
+
+        {
+            Console.Write($".");
+            Thread.Sleep(tempo);
+        
+        }
+
+        }
+
+
+
 
     }
 
